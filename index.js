8da63ce3cd2fa057daeed69cf9696ed3c7b7239b
uwtrigeo <115599976+uwtrigeo@users.noreply.github.com>
2022-11-03 09:18:50 -0700

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var map = L.map('map').setView([47.25, -122.44], 11);
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/256/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-    id: 'mapbox/dark-v10',
+    id: 'mapbox/navigation-day-v1',
     accessToken: 'pk.eyJ1IjoidHJpZ2VvIiwiYSI6ImNsOXlmZXlheTA0a3kzdmxuemE5MHVsMnQifQ.abEIHxGUIn-Yz1IwcRTT7Q',
 }).addTo(map);
 
